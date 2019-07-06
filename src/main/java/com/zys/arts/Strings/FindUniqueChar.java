f0886b4c1e241e3a0d530c11d67e9b12a5f5347b
zys <346124796@qq.com>
2019-07-06 20:27:43 +0800

    add more solution of '387. First Unique Character in a String'

--- src/main/java/com/zys/arts/Strings/FindUniqueChar.java
+++ src/main/java/com/zys/arts/Strings/FindUniqueChar.java
@@ -46,7 +46,7 @@ public class FindUniqueChar {
     }
 
 
-    public int firstUniqChar(String s) {
+    public int firstUniqChar2(String s) {
         if (s == null) {
             return -1;
         }
@@ -64,5 +64,32 @@ public class FindUniqueChar {
             }
         }
         return -1;
+    }
+
+    public static int firstUniqChar(String s) {
+        // 边界条件
+        if (s == null) {
+            return -1;
+        }
+        // 字符串长度
+        int index = s.length();
+        // 字符串是小写字母组成的字符串，所以字符范围在 a ~ z
+        for (char i = 'a'; i <= 'z'; i++) {
+            // String.indexOf（返回第一次出现指定字符的字符串中的索引，没有出现则返回 -1），所以 s.indexOf(i) != -1
+            // String.lastIndexOf(返回指定字符的最后一次出现的字符串中的索引)，所以 s.indexOf(i) == s.lastIndexOf(i) 时，该字符没有重复
+            // s.indexOf(i) < index 是要求出第一个没有重复字符
+            if (s.indexOf(i) != -1 && s.indexOf(i) == s.lastIndexOf(i) && s.indexOf(i) < index) {
+                index = s.indexOf(i);
+            }
+        }
+        // 当index != s.length() 证明存在没有重复的字符
+        if (index != s.length()) {
+            return index;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("leetcode"));
     }
 }
